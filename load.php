9539f4007ebf2f448dc6cc33a672b471271c1f21
jfsenechal <jfsenechal@gmail.com>
2022-01-13 09:26:36 +0100

    ignore load

--- load.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once WPMU_PLUGIN_DIR.'/bottin/BottinPlugin.php';
-require_once WPMU_PLUGIN_DIR.'/acsort/AcSort.php';
-require_once WPMU_PLUGIN_DIR.'/post-expiration/PostExpiration.php';
-require_once WPMU_PLUGIN_DIR.'/wordpressmarche-bottin-block-plugin/blocks/bottin-block.php';
